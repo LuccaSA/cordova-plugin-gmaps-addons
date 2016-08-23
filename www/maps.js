d1ab81af8a78fcb48e6350e06e8b79ef2c2f7222
Riron <orioncharlier@gmail.com>
2016-08-23 17:35:25 +0200

    MOD: Jamais d'espace dans les waypoints

--- www/maps.js
+++ www/maps.js
@@ -2,21 +2,23 @@ var argscheck = require('cordova/argscheck'),
     utils = require('cordova/utils'),
     exec = require('cordova/exec');
 
-var Maps = function() {
+var Maps = function () {
 };
 
-Maps.autocomplete = function(queryString, successCallback, errorCallback) {
-	argscheck.checkArgs('S', 'Maps.autocomplete', arguments);
+Maps.autocomplete = function (queryString, successCallback, errorCallback) {
+    argscheck.checkArgs('S', 'Maps.autocomplete', arguments);
     exec(successCallback, errorCallback, "Maps", "autocomplete", [queryString]);
 };
 
-Maps.geocode = function(placeId, successCallback, errorCallback) {
-	argscheck.checkArgs('S', 'Maps.geocode', arguments);
+Maps.geocode = function (placeId, successCallback, errorCallback) {
+    argscheck.checkArgs('S', 'Maps.geocode', arguments);
     exec(successCallback, errorCallback, "Maps", "geocode", [placeId]);
 };
 
-Maps.directions = function(waypoints, routeParams, successCallback, errorCallback) {
-	argscheck.checkArgs('AO', 'Maps.directions', arguments);
+Maps.directions = function (waypoints, routeParams, successCallback, errorCallback) {
+    argscheck.checkArgs('AO', 'Maps.directions', arguments);
+
+    waypoints = waypoints.map(function (v) { return v.split(' ').join('+'); })
     exec(successCallback, errorCallback, "Maps", "directions", [waypoints, routeParams]);
 };
 
